6123bd7359c34a3c0f61bb0c8d71a1dc1d46a412
youomni <58968741+youomni@users.noreply.github.com>
2026-08-30 14:57:38 +0300

    1

--- api/chat.js
+++ api/chat.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" +
-        process.env.GOOGLE,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + 
+       process.env.GOOGLE,
       {
         method: "POST",
         headers: {
